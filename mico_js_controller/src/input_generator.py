68fa78dae62d8aab098e24ebd8c08046179910e4
mu-777 <ryosuke.murata.7@gmail.com>
2015-04-02 09:44:12 +0900

    add launch files

--- mico_js_controller/src/input_generator.py
+++ mico_js_controller/src/input_generator.py
@@ -8,12 +8,12 @@ from tf.transformations import euler_from_quaternion
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Pose, PoseStamped, Twist, TwistStamped
 from std_msgs.msg import Header
-from mico_js_controller.srv import *
+from mico_js_controller.srv import InputMethodSwitchService
 
 DEBUG = True
 
 
-class InputGenerator:
+class InputGenerator(object):
     __metaclass__ = ABCMeta
 
     # able to remap names
@@ -22,25 +22,36 @@ class InputGenerator:
     current_pose_topicname = "current_PoseStamped"
     inout_method_switching_servicename = 'input_method_switcher'
 
-    def __init__(self, topic_name, topic_class):
+    def __init__(self, data_from_device_topic_name, data_from_device_topic_class):
         self.current_pose = Pose()
         self.input_pose = Pose()
         self.input_twist = Twist()
+        self.init_input_values()
+
         self.twist_mode_flag = True
-        self.linear_mode_flag = True
+        self.position_mode_flag = True
 
-        self.topic_name = topic_name
-        self.topic_class = topic_class
+        self.topic_name = data_from_device_topic_name
+        self.topic_class = data_from_device_topic_class
 
-    def initialize_pub_input(self):
+    def init_input_values(self):
+        self.input_twist.linear.x = 0
+        self.input_twist.linear.y = 0
+        self.input_twist.linear.z = 0
+        self.input_twist.angular.x = 0
+        self.input_twist.angular.y = 0
+        self.input_twist.angular.z = 0
+
+    def initialize_publishers(self):
         self.pub_pose = rospy.Publisher(self.input_pose_topicname, PoseStamped, queue_size=10)
         self.pub_twist = rospy.Publisher(self.input_twist_topicname, TwistStamped, queue_size=10)
 
-    def initialize_sub_current_pose(self):
+    def initialize_subscriber(self):
         self.sub_pose = rospy.Subscriber(self.current_pose_topicname, PoseStamped, self.ee_pose_callback)
         self.sub_data_from_device = rospy.Subscriber(self.topic_name, self.topic_class, self.data_from_device_callback)
 
     def ee_pose_callback(self, ee_pose):
+        print ee_pose
         self.current_pose = ee_pose.pose
 
     def data_from_device_callback(self, data):
@@ -50,7 +61,8 @@ class InputGenerator:
             self.generate_pose(data)
 
     def run_input_method_switch_server(self):
-        rospy.Service(self.inout_method_switching_servicename, InputMethodSwitchService, self.input_method_switch_handler)
+        rospy.Service(self.inout_method_switching_servicename, InputMethodSwitchService,
+                      self.input_method_switch_handler)
 
     @abstractmethod
     def generate_pose(self, data_from_device):
@@ -74,16 +86,16 @@ class InputGenerator:
             self.twist_mode_flag = False
 
         if req.pos_ori_switcher == 1:
-            self.linear_mode_flag = True
+            self.position_mode_flag = True
         elif req.pos_ori_switcher == 2:
-            self.linear_mode_flag = False
+            self.position_mode_flag = False
 
-    def switch_to_twist_input(self, _header):
+    def switch_twist_input(self, _header):
         self.pub_pose.unregister()
         self.pub_twist = rospy.Publisher(self.input_twist_topicname, TwistStamped, queue_size=10)
         self.pub_twist.publish(TwistStamped(header=_header, twist=self.input_twist))
 
-    def switch_to_pose_input(self, _header):
+    def switch_pose_input(self, _header):
         self.pub_twist.unregister()
         self.pub_pose = rospy.Publisher(self.input_pose_topicname, PoseStamped, queue_size=10)
         self.pub_pose.publish(PoseStamped(header=_header, pose=self.input_pose))
@@ -94,6 +106,7 @@ class JoyStickInputGen(InputGenerator):
         super(JoyStickInputGen, self).__init__('joystick', Joy)
         self.scale_pose = Pose()
         self.scale_twist = Twist()
+        self.init_input_values()
         self.set_scale_values()
 
     def set_scale_values(self):
@@ -104,15 +117,15 @@ class JoyStickInputGen(InputGenerator):
         self.scale_twist.linear.x = 0.10
         self.scale_twist.linear.y = 0.10
         self.scale_twist.linear.z = 0.10
-        self.scale_twist.angular.x = 0.10
-        self.scale_twist.angular.y = 0.10
-        self.scale_twist.angular.z = 0.10
+        self.scale_twist.angular.x = 1.0
+        self.scale_twist.angular.y = 1.0
+        self.scale_twist.angular.z = 1.0
 
     def generate_pose(self, joy_data):
         self.input_pose = self.current_pose
 
     def generate_twist(self, joy_data):
-        if self.linear_mode_flag:
+        if self.position_mode_flag:
             self.input_twist.linear.x = self.scale_twist.linear.x * joy_data.axes[3]
             self.input_twist.linear.y = self.scale_twist.linear.y * joy_data.axes[2]
             self.input_twist.linear.z = self.scale_twist.linear.z * joy_data.axes[5]
@@ -121,29 +134,29 @@ class JoyStickInputGen(InputGenerator):
             self.input_twist.angular.y = self.scale_twist.angular.y * joy_data.axes[5]
             self.input_twist.angular.z = self.scale_twist.angular.z * joy_data.axes[4]
 
-        if (DEBUG): print 'x_vel: ' + str(self.input_twist.linear.x)
-
-
+# -------------------------------------------------------------------------
 if __name__ == '__main__':
     rospy.init_node('input_generator', anonymous=True)
     rate_mgr = rospy.Rate(10)
 
     js_input_gen = JoyStickInputGen()
     try:
-        js_input_gen.initialize_pub_input()
-        js_input_gen.initialize_sub_current_pose()
+        js_input_gen.initialize_publishers()
+        js_input_gen.initialize_subscriber()
         _header = Header()
 
         js_input_gen.run_input_method_switch_server()
+
+        print('Run input_gen')
         while not rospy.is_shutdown():
             _header.stamp = rospy.Time.now()
             if js_input_gen.twist_mode_flag:
-                js_input_gen.switch_to_twist_input(_header)
+                js_input_gen.switch_twist_input(_header)
             else:
-                js_input_gen.switch_to_pose_input(_header)
+                js_input_gen.switch_pose_input(_header)
             rate_mgr.sleep()
-    except rospy.ROSInterruptException:
-        pass
+    except rospy.ROSInterruptException, e:
+        print("fail!!: " + str(e))
 
 
 
--- mico_js_controller/src/service_caller.py
+++ mico_js_controller/src/service_caller.py
@@ -1,3 +1,102 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from abc import ABCMeta, abstractmethod
+
+import rospy
+from sensor_msgs.msg import Joy
+from jaco_msgs.srv import Start, Stop, HomeArm
+from mico_js_controller.srv import InputMethodSwitchService
+
+
+class ServiceCaller(object):
+    __metaclass__ = ABCMeta
+
+    start_arm_servicename = 'start_arm'
+    stop_arm_servicename = 'stop_arm'
+    home_arm_servicename = 'home_arm'
+    switch_mode_servicename = 'switch_mode'
+
+    def __init__(self, data_from_device_topic_name, data_from_device_topic_class):
+        self.topic_name = data_from_device_topic_name
+        self.topic_class = data_from_device_topic_class
+
+        print('Declare service_caller')
+        rospy.wait_for_service(self.start_arm_servicename)
+        rospy.wait_for_service(self.stop_arm_servicename)
+        rospy.wait_for_service(self.home_arm_servicename)
+        rospy.wait_for_service(self.switch_mode_servicename)
+
+        self.start_arm_srv = rospy.ServiceProxy(self.start_arm_servicename, Start)
+        self.stop_arm_srv = rospy.ServiceProxy(self.stop_arm_servicename, Stop)
+        self.home_arm_srv = rospy.ServiceProxy(self.home_arm_servicename, HomeArm)
+        self.switch_mode_srv = rospy.ServiceProxy(self.switch_mode_servicename, InputMethodSwitchService)
+        print('Declared service_caller')
+
+    def initialize_subscriber(self):
+        self.sub_data_from_device = rospy.Subscriber(self.topic_name, self.topic_class, self.data_from_device_callback)
+
+    def data_from_device_callback(self, data):
+        print('subscribe!')
+        self.call_service(data)
+
+    @abstractmethod
+    def call_service(self, data):
+        pass
+
+
+class JoyStickSrvCaller(ServiceCaller):
+    def __init__(self):
+        super(JoyStickSrvCaller, self).__init__('joystick', Joy)
+        self.can_switch_mode = True
+        self.can_start_arm = True
+
+        self.twist_pos_switch_cnt = 0
+        self.pos_ori_switch_cnt = 0
+
+    def call_service(self, joy_data):
+        print(str(joy_data.buttons[8]) + str(joy_data.buttons[9]))
+
+        if (self.can_switch_mode is True) and joy_data.buttons[11] == 1:
+            self.pos_ori_switch_cnt += 1
+            self.switch_mode_srv((self.twist_pos_switch_cnt % 2) + 1, (self.pos_ori_switch_cnt % 2) + 1)
+            self.can_switch_mode = False
+        elif (self.can_switch_mode is False) and joy_data.buttons[11] == 0:
+            self.can_switch_mode = True
+
+        if (self.can_start_arm is True) and joy_data.buttons[8] == 0 and joy_data.buttons[9] == 1:
+            self.start_arm_srv()
+            self.can_start_arm = False
+        elif (self.can_start_arm is False) and joy_data.buttons[8] == 0 and joy_data.buttons[9] == 1:
+            self.stop_arm_srv()
+            self.can_start_arm = True
+        elif (self.can_start_arm is False) and joy_data.buttons[8] == 1 and joy_data.buttons[9] == 1:
+            self.home_arm_srv()
+
+# -------------------------------------------------------------------------
+if __name__ == "__main__":
+    rospy.init_node('service_caller')
+    js_srv_caller = JoyStickSrvCaller()
+
+    try:
+        js_srv_caller.initialize_subscriber()
+        print('Run service_caller')
+        rospy.spin()
+    except rospy.ServiceException, e:
+        print("fail!!: " + str(e))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
